30eac4e3dd3bb9974696c5970f67779cad50a4d4
Daniel Dvořák <dv.daniel@centrum.cz>
2014-12-30 16:14:47 +0100

    lm

--- build/lib/funkce.py
+++
@@ -1,4 +0,0 @@
-def lm():
-	print('lm')
-	
-	
--- stats.py
+++ stats.py
@@ -1,4 +1,23 @@
-def lm():
-	print('lm')
-	
-	
+import statsmodels.api as sm
+import patsy
+
+def lm(formula, data):
+  """
+  this function takes a patsy formula and
+  a pandas dataframe. the names of variables
+  in the formula are columns of the dataframe
+  """
+  y, X = patsy.dmatrices(formula, data, return_type='dataframe')
+  results = sm.OLS(y, X).fit()
+  print(results.summary())
+  return results
+
+
+def data(dataname = None, package = None, cache = False):
+  """
+  loads R dataset called 'dataname' from package called 'package'
+  """
+  #if dataname == None and data == None:
+  #  from rpy2.robjects import r
+  #  print(r.data())
+  return sm.datasets.get_rdataset(dataname = dataname, package = package, cache = cache).data
